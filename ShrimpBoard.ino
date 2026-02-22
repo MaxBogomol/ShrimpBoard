e91132a5ff1b2cdd91aabcb80ff16f1779c5954d
MaxBogomol <maxglotov159@gmail.com>
2026-02-22 20:39:26 +0500

    waww

--- ShrimpBoard.ino
+++ ShrimpBoard.ino
@@ -12,21 +12,6 @@
 #include <Wire.h>
 #include <SPI.h>
 
-/*
-ESP32-S3
-Flash Size: "16MB (128Mb)"
-Partition Scheme: "16M Flash (3MB APP/9.9MB FATFS)"
-PSRAM: "OPI PSRAM"
-
-esp32 - 3.3.0v
-
-Touchpad cable
-1 - GND
-2 - VDD
-4 - SDA
-5 - SCL
-*/
-
 //Pins
 #define MAX_ANALOG_VALUE 4095
 
